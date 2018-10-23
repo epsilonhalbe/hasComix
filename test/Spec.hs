a5697290daa6ff335fa3548b70ab9acfde1ae849
Martin Heuschober (TSA) <martin.heuschober@t-systems.at>
2018-10-23 19:16:31 +0200

    EX2: implement a test
    - use overloaded lists, which allow to write `[stuff]` instead of `import Data.Vector as V and then V.fromList [stuff]`

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists #-}
+
+import Data.Time.Calendar
+import Test.Hspec (hspec, it, describe)
+import Test.Hspec.Expectations.Pretty
+
+import Data.Csv
+import Comix.Data
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+  describe "tests for parsing" $
+    it "should parse a simple example" $
+      let txt = "Mike Mignola,Mike Mignola,,128,Hellboy: Seed of Destruction,2004-03-30,Dark Horse Books,*****"
+          res = Comic { comicAuthor = "Mike Mignola"
+                      , comicArtist = Just "Mike Mignola"
+                      , comicLetterer = Nothing
+                      , comicPages    = Just 128
+                      , comicTitle    = "Hellboy: Seed of Destruction"
+                      , comicPublished = fromGregorian 2004 03 30
+                      , comicPublisher = "Dark Horse Books"
+                      , comicRating = VeryGood
+                      }
+      in undefined
