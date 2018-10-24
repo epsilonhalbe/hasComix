4838822e996459a217fe427c18382f1ab369766b
Martin Heuschober (TSA) <martin.heuschober@t-systems.at>
2018-10-24 17:26:51 +0200

    add second test case
    - move from String input to Bytestring - as this is the input used in the application (OverloadedStrings) makes this easy to do, but also easy to miss

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,15 +4,17 @@
 import Data.Time.Calendar
 import Test.Hspec (hspec, it, describe)
 import Test.Hspec.Expectations.Pretty
+import Data.ByteString.Lazy
+import Data.Text.Encoding
 
 import Data.Csv
 import Comix.Data
 
 main :: IO ()
 main = hspec $
-  describe "tests for parsing" $
+  describe "tests for parsing" $ do
     it "should parse a simple example" $
-      let txt = "Mike Mignola,Mike Mignola,,128,Hellboy: Seed of Destruction,2004-03-30,Dark Horse Books,*****"
+      let txt = fromStrict $ encodeUtf8  "Mike Mignola,Mike Mignola,,128,Hellboy: Seed of Destruction,2004-03-30,Dark Horse Books,*****"
           res = Comic { comicAuthor = "Mike Mignola"
                       , comicArtist = Just "Mike Mignola"
                       , comicLetterer = Nothing
@@ -23,3 +25,17 @@ main = hspec $
                       , comicRating = VeryGood
                       }
       in decode NoHeader txt `shouldBe` Right [res]
+    it "should parse a complex example" $
+      let txt = fromStrict $ encodeUtf8 "Kōhei Horikoshi,,,0,\"僕のヒーローアカデミア, Boku no Hīrō Akademia\",2014-07-07,Shueisha,****"
+          res = Comic { comicAuthor = "Kōhei Horikoshi"
+                      , comicArtist = Nothing
+                      , comicLetterer = Nothing
+                      , comicPages    = Just 0
+                      , comicTitle    = "僕のヒーローアカデミア, Boku no Hīrō Akademia"
+                      , comicPublished = fromGregorian 2014 07 07
+                      , comicPublisher = "Shueisha"
+                      , comicRating = Good
+                      }
+      in decode NoHeader txt `shouldBe` Right [res]
+
+
